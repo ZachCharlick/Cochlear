39cb9a222468449026b4ff90358e5e7f2ce864b2
Zach Charlick <zachary.charlick@duke.edu>
2023-06-05 09:00:00 -0400

    Initializing Repo with Current Local Work

---
+++ Scripts/Logorithmic_Spacing_for_fixed_intervals.m
@@ -0,0 +1,17 @@
+number_of_intervals = 4; 
+
+minimum_freq = 1;
+maximum_freq = 4000;
+
+log_min = log10(minimum_freq);
+log_max = log10(maximum_freq);
+
+start_of_each_interval = logspace(log_min, log_max, number_of_intervals + 1);
+
+interval_bandcenters = zeros(1,number_of_intervals);
+interval_bandwidths = zeros(1,number_of_intervals);
+
+for i = (1:1:number_of_intervals)
+   interval_bandwidths(1, i) = start_of_each_interval(1, i + 1) - start_of_each_interval(1, i);
+   interval_bandcenters(1, i) = start_of_each_interval(1, i + 1) - 0.5 * interval_bandwidths(1, i);
+end
