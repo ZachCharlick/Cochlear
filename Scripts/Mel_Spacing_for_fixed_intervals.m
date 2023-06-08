52522329dd13cd51a5f73bd167bc76a775ae93bd
Zach Charlick <zsc5@duke.edu>
2023-06-08 12:07:09 -0400

    Finalized all fixed channel models, and ran the simulations to generate output audio files

--- Scripts/Mel_Spacing_for_fixed_intervals.m
+++ Scripts/Mel_Spacing_for_fixed_intervals.m
@@ -1,20 +1,37 @@
-% Gives bandcenters in Hz
-% Gives bandedges in rad/s
+number_of_intervals = 20; 
 
-number_of_intervals = 6; 
+start = 250;
+stop = 6000;
 
-minimum_freq = 250  * 2 * pi;
-maximum_freq = 6000 * 2 * pi;
-
-log_min = log10(minimum_freq);
-log_max = log10(maximum_freq);
-
-start_of_each_interval = logspace(log_min, log_max, number_of_intervals + 1);
+start_of_each_interval = zeros(1, number_of_intervals + 1);
+start_of_each_interval(1) = start;
 
 interval_bandcenters = zeros(1,number_of_intervals);
 interval_bandwidths = zeros(1,number_of_intervals);
 
+mstart = mel(start);
+
+ydif = (mel(stop) - mstart)/number_of_intervals;
+
+bandedges = [];
+
+for i = (1:1:number_of_intervals)
+    start_of_each_interval(i+1) = reversemel(mstart+i*ydif);
+end
+
 for i = (1:1:number_of_intervals)
    interval_bandwidths(1, i) = start_of_each_interval(1, i + 1) - start_of_each_interval(1, i);
-   interval_bandcenters(1, i) = (start_of_each_interval(1, i + 1) - 0.5 * interval_bandwidths(1, i))/(2 * pi);
+   interval_bandcenters(1, i) = (start_of_each_interval(1, i + 1) - 0.5 * interval_bandwidths(1, i)); % in hz
+end
+
+for i = (1:1:number_of_intervals + 1)
+   start_of_each_interval(1, i) = start_of_each_interval(1, i) * 2 * pi;
+end
+
+function m = mel(f)
+    m = 1000*log10(f/800 + 1);
+end
+
+function f = reversemel(m)
+    f = (10^(m/1000)-1)*800;
 end
